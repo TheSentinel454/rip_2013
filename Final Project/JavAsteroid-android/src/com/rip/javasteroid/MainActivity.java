588a9dd15ebe657a8804ea34f80fb4797914393b
Luke Tornquist <Luke.Tornquist@gmail.com>
2013-11-09 10:59:20 -0500

    Finalize IntelliJ settings

--- Final Project/JavAsteroid-android/src/com/rip/javasteroid/MainActivity.java
+++ Final Project/JavAsteroid-android/src/com/rip/javasteroid/MainActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-public class MainActivity extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
-        
-        initialize(new JavAsteroid(), cfg);
-    }
+public class MainActivity extends AndroidApplication
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+		cfg.useGL20 = true;
+		cfg.useAccelerometer = false;
+		cfg.useCompass = false;
+		cfg.useWakelock = true;
+
+		initialize(new JavAsteroid(), cfg);
+	}
 }
--- Final Project/JavAsteroid/src/com/rip/javasteroid/JavAsteroid.java
+++ Final Project/JavAsteroid/src/com/rip/javasteroid/JavAsteroid.java
@@ -17,7 +17,7 @@ public class JavAsteroid implements ApplicationListener {
 	private Sprite sprite;
 	
 	@Override
-	public void create() {		
+	public void create() {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		

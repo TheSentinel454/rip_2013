a82797756e1b0e93f33d885c0741464bfc05b12f
romeconquerer <steffan.slater@gmail.com>
2013-12-06 15:34:20 -0500

    Calculate safe headings
    Sorry about the ternary operators. I think I have a problem.
    
    Still need to merge safe headings after each asteroid

--- Final Project/Planner/src/Main.java
+++ Final Project/Planner/src/Main.java
@@ -10,6 +10,7 @@ import com.rip.javasteroid.remote.RmiServer;
 
 import java.rmi.Naming;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main
@@ -109,10 +110,10 @@ public class Main
 
 			for(int ndx = 0; ndx < deltaV.length ; ndx++)
 			{
-                delta_theta[ndx] = (float)(Math.PI * ((float)(ndx/granularity) - 1));
+                delta_theta[ndx] = (360.0f * ndx)/(2*granularity);
                 deltaV[ndx] = new Vector2(1f,0f);
-                deltaV[ndx].setAngle(delta_theta[ndx]);
-                deltaV[ndx].scl(Ship.SHIP_LINEAR_ACCELERATION * (delta_t - Math.abs(delta_theta[ndx]) / Ship.SHIP_ANGULAR_VELOCITY));
+                deltaV[ndx].rotate(delta_theta[ndx]);
+                deltaV[ndx].scl(Ship.SHIP_LINEAR_ACCELERATION * (delta_t - (float)Math.toRadians(delta_theta[ndx] < 180 ? delta_theta[ndx] : Math.abs(delta_theta[ndx]-360.0f)) / Ship.SHIP_ANGULAR_VELOCITY));
             }
 
 			for(EntityData asteroid : asteroids)
@@ -132,10 +133,10 @@ public class Main
 				{
 					//Calculate occlusion points
 					occ_point[0] = new Vector2(1f,0f);
-					occ_point[0].rotate((float)(-1*Math.asin(config_radius/relpos.len())*180/Math.PI));
+					occ_point[0].rotate((float)Math.toDegrees(-1*Math.asin(config_radius/relpos.len())));
 					occ_point[0].scl((float)(Math.sqrt(relpos.len() * relpos.len() - config_radius * config_radius)));
 					occ_point[1] = new Vector2(occ_point[0]);
-					occ_point[1].rotate((float)(2*Math.asin(config_radius/relpos.len())*180/Math.PI));
+					occ_point[1].rotate((float)(Math.toDegrees(2*Math.asin(config_radius/relpos.len()))));
 
 					//Calculate relative velocity
 					relv = new Vector2(asteroid.getVelocity());
@@ -148,12 +149,10 @@ public class Main
 						theta[ndx] = reachableV[ndx].angle();
 					}
 
-					//Determine excluded headings from delta-V ellipse based on occlusion points of velocity obstacle
+					//Determine excluded headings from delta-V curve based on occlusion points of velocity obstacle
 					//Excluded headings come from intersection of occlusion point rays with curve - rays are theta = constant
-					//Find thetas on delta-V curve bracketing ray theta, linearly interpolate to find intersection vector
+					//Find thetas on delta-V curve bracketing ray theta, interval bisection to find intersection vector
 					//Up to 4 intersections per ray (0-8 per obstacle)
-					//To determine headings, translate vector back to origin (subtract relv), rotate back to x-axis (rotate -ship.angle)
-					//use getAngle to get headings of intersection points
 
                     ArrayList<Float> occ_intersect = new ArrayList<Float>();
 
@@ -167,12 +166,14 @@ public class Main
                     }
 
                     // Locate crossings by finding sign changes of theta - occlusion_theta
+                    float prev;
                     for(int occ_ndx = 0; occ_ndx < 2; occ_ndx++) {
-                        float prev = Math.signum(theta_diff[occ_ndx][0]);
+                        prev = Math.signum(theta_diff[occ_ndx][0]);
                         for(int ndx = 0; ndx < theta_diff.length; ndx++) {
                             if(theta_diff[occ_ndx][ndx] == 0) {
                                 occ_intersect.add(new Float(delta_theta[ndx]));
                             } else if(Math.signum(theta_diff[occ_ndx][ndx]) != prev) {
+                                //TODO: Convert to zero-finding with interval bisection
                                 float interp = (delta_theta[ndx] - delta_theta[ndx-1]) / (theta_diff[occ_ndx][ndx] - theta_diff[occ_ndx][ndx-1]) * theta_diff[occ_ndx][ndx] + delta_theta[ndx];
                                 occ_intersect.add(new Float(interp));
                             }
@@ -181,7 +182,29 @@ public class Main
                     }
 
                     //March around delta_theta, at each occlusion heading check if it starts or ends an exclusion zone
-                    //Store as bitmask - 0 = not excluded, 1 = excluded
+                    //Store as bitmask - 0 = not safe, 1 = safe
+                    Collections.sort(occ_intersect);
+                    if(occ_intersect.get(occ_intersect.size()-1) == 360.0f) {
+                        occ_intersect.remove(occ_intersect.size()-1);
+                    }
+                    boolean[] safe = new boolean[occ_intersect.size()+1];
+
+                    float inc_angle = occ_point[1].angle() - occ_point[0].angle() + ((occ_point[1].angle() - occ_point[0].angle() >= 0) ? (0) : (360.0f));
+
+                    for(int ndx = 0; ndx < safe.length; ndx++) {
+                        float ang = 0.5f * (((ndx == 0) ? (0.0f) : (occ_intersect.get(ndx))) + ((ndx+1 == safe.length) ? (360.0f) : (occ_intersect.get(ndx+1))));
+
+                        Vector2 dv = new Vector2(1f,0f);
+                        dv.rotate(ang);
+                        dv.scl(Ship.SHIP_LINEAR_ACCELERATION * (delta_t - (float)Math.toRadians(ang < 180 ? ang : Math.abs(ang-360.0f)) / Ship.SHIP_ANGULAR_VELOCITY));
+                        dv.rotate(ship.getAngle()).add(relv);
+
+                        ang = occ_point[1].angle() - dv.angle() + ((occ_point[1].angle() - dv.angle() >= 0) ? (0) : (360.0f));
+                        ang += dv.angle() - occ_point[1].angle() + ((dv.angle() - occ_point[1].angle() >= 0) ? (0) : (360.0f));
+
+                        //Safe if sum of angles is greater than angle between occlusion points
+                        safe[ndx] = ang > inc_angle;
+                    }
 
 					//Store excluded headings
 
